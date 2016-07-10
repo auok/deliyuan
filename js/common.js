010355cadb1e05867666f0c02be59d9bf4d00b58
uustoboy <tianqi_email@163.com>
2016-07-10 18:16:56 +0800

    add

--- js/common.js
+++ js/common.js
@@ -7,14 +7,14 @@ $(function(){
 		var index = $this.index();
 		var btn = $(this).attr('btn');
 		if( btn == '0' ){
-			$this.attr('btn',1);
+			$this.attr('btn',1).siblings('a').attr('btn',0);
 
 			$this.addClass('pro-tabCur').siblings('a').removeClass('pro-tabCur');
 			$('.help-lists').eq(index).addClass('show').siblings('.help-lists').removeClass('show');
 		}else{
 			$('.help-lists').removeClass('show');
 			$('.pro-btns a').removeClass('pro-tabCur');
-			$('.pro-btns a').attr('btn',0);
+			$this.attr('btn',0);
 		}
 		
 	});
